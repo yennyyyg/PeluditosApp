583d8a6ae7b65d944547b171734f0dc7f03ce3b2
cdgr1 <cdgr16@gmail.com>
2022-05-23 18:45:47 -0500

    storage

--- app/src/main/java/com/ean/peluditosapp/Deseas_adoptar.kt
+++ app/src/main/java/com/ean/peluditosapp/Deseas_adoptar.kt
@@ -1,6 +1,5 @@
 package com.ean.peluditosapp
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,46 +14,27 @@ import com.google.firebase.storage.ktx.storage
 class Deseas_adoptar : AppCompatActivity() {
     // Create a storage reference from our app
     val storage = Firebase.storage
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deseas_adoptar)
-        val imagenesperro=ArrayList<String>()
-        val imagenesgato=ArrayList<String>()
+        val storageRef=storage.reference
+        val imagenes=ArrayList<String>()
         val listRef=storage.reference.child("caninos")
         val listFelinos=storage.reference.child("felinos")
-        val listaImg=findViewById<ListView>(R.id.list_view_peluditos)
-        val boton_Perro=findViewById<RadioButton>(R.id.chb_perro)
-        boton_Perro.setOnClickListener {
-            listRef.listAll()
-                .addOnSuccessListener {
-                    for (i in it.items){
-                        imagenesperro.add(i.name+"")
-                    }
-                    Log.d("Firebase","files $imagenesperro")
+        listRef.listAll()
+            .addOnSuccessListener {
+                for (i in it.items){
+                    imagenes.add(i.name+"")
                 }
-                .addOnFailureListener{
-                    Log.d("Firebase","error $it")
-                }
-
-            val adapImg=ArrayAdapter(this,android.R.layout.simple_list_item_1,imagenesperro)
-            listaImg.adapter=adapImg
-        }
-
-        val boton_Gato=findViewById<RadioButton>(R.id.chb_gato)
-        boton_Gato.setOnClickListener {
-            listFelinos.listAll()
-                .addOnSuccessListener {
-                    for (i in it.items){
-                        imagenesgato.add(i.name+"")
-                    }
-                    Log.d("Firebase","files $imagenesgato")
-                }
-                .addOnFailureListener{
-                    Log.d("Firebase","error $it")
-                }
-
-            val adapImg=ArrayAdapter(this,android.R.layout.simple_list_item_1,imagenesgato)
+                Log.d("Firebase","files $imagenes")
+            }
+            .addOnFailureListener{
+                Log.d("Firebase","error $it")
+            }
+        val boton_Refre=findViewById<Button>(R.id.btn_refrescar)
+        boton_Refre.setOnClickListener {
+            val listaImg=findViewById<ListView>(R.id.list_view_peluditos)
+            val adapImg=ArrayAdapter(this,android.R.layout.simple_list_item_1,imagenes)
             listaImg.adapter=adapImg
         }
 
